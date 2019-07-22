f21d213b13a924ee58edbe1d40485540e98e115a
kuppalla9 <kuppalla9@gmail.com>
2019-07-22 15:57:39 +0530

    added new line in hellowrld

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
       // Prints "Hello, I am here in the terminal window.
       System.out.println("Hello all, Welcome to Winning Devops Team");
       System.out.println("Moinng code from eclips to git hub repo");
+      System.out.println("code added");
    }
 }
