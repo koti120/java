6bb3b5544687d8c184c169407697de425f5f922b
kuppalla9 <kuppalla9@gmail.com>
2019-07-22 11:02:56 +0530

    fist eclips program

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
       System.out.println("Hello all, Welcome to Winning Devops Team");
+      System.out.println("Moinng code from eclips to git hub repo");
    }
 }
